d7f96d9428036fdd05ef1f9bc8ea326e2f6fdc84
varsha bavane <varshab13bavane@gmail.com>
2018-03-01 10:47:43 +0530

    ionIem and viewItem functions are called in ts file n one more fuction is created which is ionviewdidLoad which shown item

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+
+  ionViewdidLoad(){
+    this.items = [
+      {title: 'hi', description:'here you are'},
+      {title:'hello', description:'hey'},
+    {title:'hey',description:'jyane'}
+
+    ]
+  }
+  addIem(){
+    alert('add item works')
+  }
+
+  viewItem(){
+    alert('this is your item' + JSON.stringify(item))
   }
 
 }
