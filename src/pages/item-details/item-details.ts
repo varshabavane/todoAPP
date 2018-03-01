3b2645add7ea35daa947ebe18b02062bbac529ba
varsha bavane <varshab13bavane@gmail.com>
2018-03-01 19:44:55 +0530

    third page created as itemdetails and imported module in appmodule

---
+++ src/pages/item-details/item-details.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ItemDetailsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-item-details',
+  templateUrl: 'item-details.html',
+})
+export class ItemDetailsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ItemDetailsPage');
+  }
+
+}
